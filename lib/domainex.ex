a64ea73fc5e007a88582512e960bb1b4b6577b14
Hiraq <hiraq.dev@gmail.com>
2023-08-08 08:42:20 +0700

    init project

---
+++ lib/domainex.ex
@@ -0,0 +1,18 @@
+defmodule Domainex do
+  @moduledoc """
+  Documentation for `Domainex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Domainex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/domainex_test.exs
@@ -0,0 +1,8 @@
+defmodule DomainexTest do
+  use ExUnit.Case
+  doctest Domainex
+
+  test "greets the world" do
+    assert Domainex.hello() == :world
+  end
+end
