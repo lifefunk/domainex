a1df1f1f078f6ff27457922cea99d31d584081d5
Hiraq <hiraq.dev@gmail.com>
2023-08-11 03:26:05 +0700

    refactor(aggregate): adding helper for aggregate

--- lib/domainex/aggregate.ex
+++ lib/domainex/aggregate.ex
@@ -10,6 +10,16 @@ defmodule Domainex.Aggregate do
   and only provide some helpers
   """
   alias Domainex, as: BaseType
+  alias Domainex.Common, as: Common
+
+  @error_invalid_aggregate_type "given data is not aggregate type"
+  @error_invalid_data_type "unknown given data type"
+
+  @spec error_invalid_aggregate_type() :: binary()
+  def error_invalid_aggregate_type, do: @error_invalid_aggregate_type
+
+  @spec error_invalid_data_type() :: binary()
+  def error_invalid_data_type, do: @error_invalid_data_type
 
   defmodule Structure do
     @moduledoc """
@@ -28,34 +38,121 @@ defmodule Domainex.Aggregate do
     defstruct [:contains, :events]
 
     @type t :: %__MODULE__{
-      contains: struct() | list(struct()),
+      contains: struct() | %{atom() => struct()},
       events: list(BaseType.event())
     }
   end
 
   @doc """
-  A `new/1` has two possibilities depends on given parameter.
+  A `new/2` has two possibilities depends on given parameter.
   If it give a single entity which is a `struct()` it will generate a
-  `Structure.t()` that contains a single entity.
+  `BaseType.aggregate()` that contains a main aggregate object with single entity,
+  or people common said as aggregate root.
 
-  If it give a list of entities, it will generate a `Structure.t()` that
+  If it give a list of entities, it will generate a `BaseType.aggregate()` that
   contains a list of entities.
 
   All generated structure will always generated with an empty `:events`
   """
-  @spec new(entity :: struct()) :: Structure.t()
-  def new(entity) when is_struct(entity) do
-    %Structure{
+  @spec new(entity :: struct(), name :: atom()) :: BaseType.aggregate()
+  def new(entity, name) when is_struct(entity) and is_atom(name) or is_binary(name) do
+    aggregate = %Structure{
       contains: entity,
       events: []
     }
+
+    {:aggregate, {name, aggregate}}
   end
 
-  @spec new(entities :: list(struct())) :: Structure.t()
-  def new(entities) when is_list(entities) do
-    %Structure{
+  @spec new(entities :: %{atom() => struct()}, name :: atom()) :: BaseType.aggregate()
+  def new(entities, name) when is_map(entities) and is_atom(name) or is_binary(name) do
+    aggregate = %Structure{
       contains: entities,
       events: []
     }
+
+    {:aggregate, {name, aggregate}}
+  end
+
+  @doc """
+  `is_aggregate?/1` used to check if given tuple is an `:aggregate` or not. For any types
+  which not a tuple, it will return false
+  """
+  @spec is_aggregate?(given :: BaseType.aggregate()) :: boolean()
+  def is_aggregate?(given) when is_tuple(given), do: Common.is_tuple_has_context?(given, :aggregate)
+  def is_aggregate?(_), do: false
+
+  @doc """
+  `aggregate/1` used to extract a main aggregate data structure from the tuple of:
+
+  ```elixir
+    # used to extract structure
+    {:aggregate, {name, structure}}
+  ```
+
+  It will return an error of `:aggregate` if given parameter is not tuple
+  """
+  @spec aggregate(data :: BaseType.aggregate()) :: BaseType.result()
+  def aggregate(data) when is_tuple(data) do
+    with true <- is_aggregate?(data),
+         {:ok, tuple} <- Common.extract_element_from_tuple(data, 1),
+         {:ok, aggregate} <- Common.extract_element_from_tuple(tuple, 1)
+    do
+      {:ok, aggregate}
+    else
+      false -> {:error, {:aggregate, @error_invalid_aggregate_type}}
+      {:error, {error_type, error_message}} -> {:error, {error_type, error_message}}
+    end
+  end
+  def aggregate(_), do: {:error, {:aggregate, @error_invalid_data_type}}
+
+  @doc """
+  `update_entity/2` used to update internal aggregate's entity. This function used
+  only for an aggregate with a single entity.
+
+  Usage:
+
+    ```elixir
+      aggregate2 = aggregate |> Aggregate.update_entity(fake_entity_updated)
+    ```
+  """
+  @spec update_entity(data :: BaseType.aggregate(), entity :: struct()) :: BaseType.result()
+  def update_entity(data, entity) when is_tuple(data) and is_struct(entity) do
+    with true <- is_aggregate?(data),
+         {:ok, tuple} <- Common.extract_element_from_tuple(data, 1),
+         {:ok, agg_name} <- Common.extract_element_from_tuple(tuple, 0),
+         {:ok, agg_object} <- data |> aggregate
+    do
+      {:ok, {:aggregate, {agg_name, %{agg_object | contains: entity}}}}
+    else
+      false -> {:error, {:aggregate, "given data is not aggregate type"}}
+      {:error, {error_type, error_msg}} -> {:error, {error_type, error_msg}}
+    end
+  end
+
+  @doc """
+  `update_entity/3` used to update one of available entities. This function used for an aggregate
+  with multiple entities.
+
+  Usage:
+
+    ```elixir
+      {:ok, aggregate2} = aggregate |> Aggregate.update_entity(:fake1, fake_entity_1_updated)
+    ```
+
+  The `update_entity/3` need a `key` to update some entity, since an aggregate that contains multiple
+  entities will be mapped based on some unique key
+  """
+  @spec update_entity(data :: tuple(), key :: atom() , entity :: struct()) :: BaseType.result()
+  def update_entity(data, key, entity) when is_tuple(data) and is_atom(key) and is_struct(entity) do
+    with true <- is_aggregate?(data),
+         {:ok, agg_name} <- Common.extract_element_from_tuple(data, 1),
+         {:ok, agg_object} <- data |> aggregate
+    do
+      {:ok, {:aggregate, {agg_name, %{agg_object | contains: Map.put(agg_object.contains, key, entity)}}}}
+    else
+      false -> {:error, {:aggregate, "given data is not aggregate type"}}
+      {:error, {error_type, error_msg}} -> {:error, {error_type, error_msg}}
+    end
   end
 end
--- test/domainex/aggregate_test.exs
+++ test/domainex/aggregate_test.exs
@@ -1,24 +1,162 @@
 defmodule Domainex.AggregateTest do
   use ExUnit.Case
+
   alias Domainex.Aggregate
+  alias Domainex.Common
+
 
   defmodule FakeEntityStruct do
     defstruct [:name]
   end
 
-  test "new/1 with single entity" do
-    fake_entity = %FakeEntityStruct{name: "fake_entity"}
-    aggregate = Aggregate.new(fake_entity)
-    assert aggregate.contains == fake_entity
-    assert length(aggregate.events) == 0
+  describe "new/1" do
+    test "with single entity" do
+      fake_entity = %FakeEntityStruct{name: "fake_entity"}
+      aggregate = Aggregate.new(fake_entity, :fake_entity)
+
+      assert is_tuple(aggregate)
+      assert elem(aggregate, 0) == :aggregate
+      assert is_tuple(elem(aggregate, 1))
+      assert elem(elem(aggregate, 1), 0) == :fake_entity
+      assert is_struct(elem(elem(aggregate, 1), 1))
+
+      agg_object = elem(elem(aggregate, 1), 1)
+      assert agg_object.contains == fake_entity
+    end
+
+    test "with map of entities" do
+      fake_entity_1 = %FakeEntityStruct{name: "fake_entity_1"}
+      fake_entity_2 = %FakeEntityStruct{name: "fake_entity_2"}
+      aggregate = Aggregate.new(%{:fake1 => fake_entity_1, :fake2 => fake_entity_2}, :fake_agg)
+
+      assert is_tuple(aggregate)
+      assert elem(aggregate, 0) == :aggregate
+      assert is_tuple(elem(aggregate, 1))
+      assert elem(elem(aggregate, 1), 0) == :fake_agg
+      assert is_struct(elem(elem(aggregate, 1), 1))
+
+      agg_object = elem(elem(aggregate, 1), 1)
+      agg_fake_1 = agg_object.contains.fake1
+      agg_fake_2 = agg_object.contains.fake2
+
+      assert agg_fake_1 == fake_entity_1
+      assert agg_fake_2 == fake_entity_2
+    end
   end
 
-  test "new/1 with list of entities" do
-    fake_entity_1 = %FakeEntityStruct{name: "fake_entity_1"}
-    fake_entity_2 = %FakeEntityStruct{name: "fake_entity_2"}
-    aggregate = Aggregate.new([fake_entity_1, fake_entity_2])
-    assert length(aggregate.contains) == 2
-    assert length(aggregate.events) == 0
+  describe "is_aggregate?/1" do
+    test "with correct tuple type" do
+      fake_entity = %FakeEntityStruct{name: "fake_entity"}
+      aggregate = Aggregate.new(fake_entity, :fake_entity)
+      assert Aggregate.is_aggregate?(aggregate)
+    end
+
+    test "with invalid tuple type" do
+      assert !Aggregate.is_aggregate?({:test, :test})
+    end
+
+    test "with invalid type" do
+      assert !Aggregate.is_aggregate?(1)
+    end
   end
 
+  describe "aggregate/1" do
+    test "using valid type" do
+      fake_entity = %FakeEntityStruct{name: "fake_entity"}
+      aggregate = Aggregate.new(fake_entity, :fake_entity)
+      {:ok, structure} = aggregate |> Aggregate.aggregate
+
+      assert structure.contains == fake_entity
+    end
+
+    test "with different tuple format" do
+      {:error, {error_type, error_message}} = Aggregate.aggregate({:test, :test})
+      assert error_type == :aggregate
+      assert error_message == Aggregate.error_invalid_aggregate_type()
+    end
+
+    test "with invalid aggregate tuple format" do
+      {:error, {error_type, error_message}} = Aggregate.aggregate({:aggregate})
+      assert error_type == :exception
+      assert String.contains?(error_message, "out of range")
+    end
+
+    test "with non-tuple type" do
+      {:error, {error_type, error_message}} = Aggregate.aggregate(1)
+      assert error_type == :aggregate
+      assert error_message == Aggregate.error_invalid_data_type()
+    end
+  end
+
+  describe "update_entity/2" do
+    test "should be success" do
+      fake_entity = %FakeEntityStruct{name: "fake_entity"}
+      fake_entity_updated = %FakeEntityStruct{name: "fake_entity_updated"}
+      aggregate = Aggregate.new(fake_entity, :fake_entity)
+
+      {:ok, structure} = aggregate |> Aggregate.aggregate
+      assert structure.contains == fake_entity
+      assert structure.contains.name == fake_entity.name
+
+      aggregate2 = aggregate |> Aggregate.update_entity(fake_entity_updated)
+      {:ok, agg_object_2} = Common.extract_element_from_tuple(aggregate2, 1)
+
+      {:ok, structure} = agg_object_2 |> Aggregate.aggregate
+      assert structure.contains == fake_entity_updated
+      assert structure.contains.name == fake_entity_updated.name
+    end
+
+    test "with invalid aggregate type" do
+      fake_entity = %FakeEntityStruct{name: "fake_entity"}
+      {:error, {error_type, error_msg}} = Aggregate.update_entity({:test, :test}, fake_entity)
+
+      assert error_type == :aggregate
+      assert error_msg == Aggregate.error_invalid_aggregate_type()
+    end
+
+    test "with invalid aggregate format" do
+      fake_entity = %FakeEntityStruct{name: "fake_entity"}
+
+      {:error, {error_type, error_message}} = Aggregate.update_entity({:aggregate}, fake_entity)
+      assert error_type == :exception
+      assert String.contains?(error_message, "out of range")
+    end
+  end
+
+  describe "update_entity/3" do
+    test "should be success" do
+      fake_entity_1 = %FakeEntityStruct{name: "fake_entity"}
+      fake_entity_2 = %FakeEntityStruct{name: "fake_entity_updated"}
+      aggregate = Aggregate.new(%{:fake1 => fake_entity_1, :fake2 => fake_entity_2}, :entities)
+
+      {:ok, structure} = aggregate |> Aggregate.aggregate
+      assert structure.contains.fake1 == fake_entity_1
+      assert structure.contains.fake1.name == fake_entity_1.name
+      assert structure.contains.fake2 == fake_entity_2
+      assert structure.contains.fake2.name == fake_entity_2.name
+
+      fake_entity_1_updated = %FakeEntityStruct{name: "fake_entity_updated"}
+      {:ok, aggregate2} = aggregate |> Aggregate.update_entity(:fake1, fake_entity_1_updated)
+      {:ok, structure} = aggregate2 |> Aggregate.aggregate
+
+      assert structure.contains.fake1 == fake_entity_1_updated
+      assert structure.contains.fake1.name == fake_entity_1_updated.name
+    end
+
+    test "with invalid aggregate type" do
+      fake_entity_1 = %FakeEntityStruct{name: "fake_entity"}
+      {:error, {error_type, error_msg}} = Aggregate.update_entity({:test, :test}, fake_entity_1)
+
+      assert error_type == :aggregate
+      assert error_msg == Aggregate.error_invalid_aggregate_type()
+    end
+
+    test "with invalid aggregate format" do
+      fake_entity_1 = %FakeEntityStruct{name: "fake_entity"}
+      {:error, {error_type, error_msg}} = Aggregate.update_entity({:aggregate}, fake_entity_1)
+
+      assert error_type == :exception
+      assert String.contains?(error_msg, "out of range")
+    end
+  end
 end
